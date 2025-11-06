7b544e5158a7c693fd9fc2bc62838d863fe95c26
Max <conr9811@gmail.com>
2025-11-06 13:29:24 +0100

    Edit color for title

--- src/pages/checkout/ShipmentInformation.jsx
+++ src/pages/checkout/ShipmentInformation.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Breadcrumb from "../../components/Breadcrumb";
+import styles from "./ShipmentInformation.module.css"
 
 export default function ShippingInformation() {
     return (
-        <div>
+        <div className={styles.container}>
             <div className={styles.breadcrumb}>
                 <Breadcrumb />
             </div>
-            <h1>Shipping Information</h1>
+            <h1 className={styles.title}>Shipping Information</h1>
         </div>
     )
 }
